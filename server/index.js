838fde0b0efec887aa1e78cdcd540cebcda6066a
unknown <zoltan.berente@outlook.com>
2018-10-31 16:13:33 -0600

    first commit

---
+++ server/index.js
@@ -0,0 +1,14 @@
+require('dotenv').config()
+const express = require('express')
+const bodyParser = require('body-parser')
+const massive =  require('massive')
+
+
+const port = process.env.PORT
+
+const app = express();
+app.use(bodyParser.json());
+
+app.listen(port, () => {
+    console.log(`Ship docked at port: ${port}`)
+})
