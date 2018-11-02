47ef5358b4b4f7747bd476c357c17dd113f1cc86
unknown <zoltan.berente@outlook.com>
2018-11-01 20:32:18 -0600

    progress

--- src/Component/Nav/Nav.js
+++ src/Component/Nav/Nav.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import Login from '../../images/Login.png';
+import Cart from '../../images/cart.png';
+import LinusBanner from '../../images/LinusBanner.png'
+import '../../App.css';
 
 const Nav = () =>{
     return(
-        <div>Nav</div>
+        <div className='Nav'>
+        <img src={LinusBanner} className='bannerImg' alt='Banner'/>
+        <img src={Login} className='navImg' alt='Login'/>
+        <img src={Cart} className='navImg' alt='Cart'/>
+        </div>
     )
 }
 
